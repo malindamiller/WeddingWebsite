ca5c2c056cf81da7df05bed8a7c7e58aee2133fb
malindamiller <mindadiann@gmail.com>
2017-02-05 14:20:21 -0500

    add semilcolons

--- server.js
+++ server.js
@@ -17,37 +17,36 @@ mongoose.connect(url);
 
 app.get('/', function (req, res) {
    res.sendFile('index.html');
-})
+});
 app.get('/rsvp', function (req, res) {
    res.sendFile(path.join(__dirname + '/public/rsvp.html'));
-})
+});
 app.get('/admin', function (req, res) {
    res.sendFile(path.join(__dirname + '/public/admin.html'));
-})
+});
 app.get('/getRSVP', function (req, res) {
   RSVP.find({}, function(err, users) {
-  if (err)
-    throw err;
-  res.send(users);
+    if (err)
+      throw err;
+    res.send(users);
+  });
 });
-})
 app.post('/rsvp', function (req, res) {
    var rsvp = new RSVP({
      name: req.body.name,
      attending: req.body.attending,
-   })
+   });
 
    rsvp.save(function (err, rsvp) {
      if(err) {
        return next(err);
      }
-   })
+   });
    res.sendFile(path.join(__dirname + '/public/confirm.html'));
-})
+});
 
-var server = app.listen(8081, function () {
-var host = server.address().address
-var port = server.address().port
-
-console.log("app listening at http://%s:%s", host, port)
-})
+ var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("app listening at http://%s:%s", host, port);
+});
