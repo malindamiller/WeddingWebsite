d545886cd256e42eab53df69b19f393d54a661d5
malindamiller <malindamiller2016@gmail.com>
2017-05-14 17:52:46 -0400

    ui changes and improvements.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser')
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var RSVP = require ('./RSVP.js');
+var RSVP = require('./RSVP.js');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -16,27 +16,28 @@ var url = 'mongodb://localhost:27017/Wedding';
 mongoose.connect(url);
 
 app.get('/', function (req, res) {
-   res.sendFile('index.html');
+  res.sendFile('index.html');
 });
 app.get('/rsvp', function (req, res) {
-   res.sendFile(path.join(__dirname + '/public/rsvp.html'));
+  res.sendFile(path.join(__dirname + '/public/rsvp.html'));
 });
 app.get('/admin', function (req, res) {
-   res.sendFile(path.join(__dirname + '/public/admin.html'));
+  res.sendFile(path.join(__dirname + '/public/admin.html'));
 });
 app.get('/getRSVP', function (req, res) {
-  RSVP.find({}, function(err, users) {
-    if (err)
+  RSVP.find({}, function (err, users) {
+    if (err) {
       throw err;
+    }
     res.send(users);
   });
 });
 app.post('/rsvp', function (req, res) {
-   var rsvp = new RSVP({
-     name: req.body.name,
-     attending: req.body.attending,
-     count: req.body.count
-   });
+  var rsvp = new RSVP({
+    name: req.body.name,
+    attending: req.body.attending,
+    count: req.body.count
+  });
 
    rsvp.save(function (err, rsvp) {
      if(err) {
